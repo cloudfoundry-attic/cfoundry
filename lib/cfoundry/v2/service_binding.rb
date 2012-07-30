f3d80b341e1435ad76cac3d519e74944049aef85
Alex Suraci <suraci.alex@gmail.com>
2012-07-30 16:04:49 -0700

    ServiceBinding#vendor_data -> #gateway_data
    Change-Id: I144d0e15032f86e20f45897d77e44832c0998b66

--- lib/cfoundry/v2/service_binding.rb
+++ lib/cfoundry/v2/service_binding.rb
@@ -4,7 +4,7 @@ module CFoundry::V2
   class ServiceBinding < Model
     attribute :credentials
     attribute :binding_options, :default => {}
-    attribute :vendor_data, :default => {}
+    attribute :gateway_data, :default => {}
     to_one    :app
     to_one    :service_instance
   end
