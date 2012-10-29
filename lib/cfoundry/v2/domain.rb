cbe4a48de76a266959f21d850d856ac1198b5444
Alex Suraci <suraci.alex@gmail.com>
2012-10-29 11:42:41 -0700

    add v2 domain 'wildcard' attribute
    Change-Id: I7abfdfbcce6b847773286d84baa3b944af337d95

--- lib/cfoundry/v2/domain.rb
+++ lib/cfoundry/v2/domain.rb
@@ -3,6 +3,7 @@ require "cfoundry/v2/model"
 module CFoundry::V2
   class Domain < Model
     attribute :name, :string
+    attribute :wildcard, :boolean, :default => true
     to_one    :owning_organization, :as => :organization
 
     scoped_to_organization :owning_organization
