680e6499b39b73e7480f67502d3473e9ba0f4e5c
Alex Suraci <suraci.alex@gmail.com>
2012-12-14 09:00:41 -0800

    fix factory girl definition finding
    Change-Id: Ibbbf5171ffe5b1bab1f547573bb46259b5abc53a

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,7 +7,9 @@ Dir[File.expand_path('../support/**/*.rb', __FILE__)].each do |file|
   require file
 end
 
-FactoryGirl.definition_file_paths = File.expand_path("../factories", __FILE__)
+FactoryGirl.definition_file_paths =
+  [File.expand_path("../factories", __FILE__)]
+
 FactoryGirl.find_definitions
 
 RSpec.configure do |c|
