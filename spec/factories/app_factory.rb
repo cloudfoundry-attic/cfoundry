2cfc09e14617f16bb0374c94f7d34a626aba53c4
Alex Suraci and Elisabeth Hendrickson <suraci.alex@gmail.com>
2012-12-13 17:21:48 -0800

    add routes/service bindings to app factory
    Change-Id: Ibd23213397b0768df2061e4982b0614363a4a760

--- spec/factories/app_factory.rb
+++ spec/factories/app_factory.rb
@@ -7,8 +7,18 @@ FactoryGirl.define do
     production false
     state "STOPPED"
 
+    ignore do
+      routes []
+      service_bindings []
+    end
+
     initialize_with do
       CFoundry::V2::App.new(nil, nil)
+    end
+
+    after_build do |app, evaluator|
+      RR.stub(app).routes { evaluator.routes }
+      RR.stub(app).service_bindings { evaluator.service_bindings }
     end
   end
 end
