ca44f4f9c152ebea7e7255f7c9f590d3b5b39fec
Alex Suraci & Chris Brown <pair+alex+cbrown@pivotallabs.com>
2013-02-04 16:47:24 -0800

    v2 domain wildcard is not optional

--- lib/cfoundry/v2/domain.rb
+++ lib/cfoundry/v2/domain.rb
@@ -3,7 +3,7 @@ require "cfoundry/v2/model"
 module CFoundry::V2
   class Domain < Model
     attribute :name, :string
-    attribute :wildcard, :boolean, :default => true
+    attribute :wildcard, :boolean
     to_one    :owning_organization, :as => :organization, :default => nil
 
     queryable_by :name, :owning_organization_guid, :space_guid
