10661a2fc545cb9a9a56cb3801db2b29559ca81b
Alex Suraci & Chris Brown <pair+alex+cbrown@pivotallabs.com>
2013-03-11 15:23:47 -0700

    fix timecop

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -18,3 +18,14 @@ RSpec.configure do |c|
   c.include V1Fake::FakeMethods
   c.mock_with :rr
 end
+
+# There is a bug in Timecop v0.6.0 that tries to use Time.zone class method
+# (from ActiveSupport) if it is defined. However, it does not go on to check
+# that it is not nil which can happen if you only use the core_ext part of
+# ActiveSupport (which we are). There is a pull request on GitHub[1] to fix
+# this but until then we're removing the zone class method.
+#
+# [1]: https://github.com/travisjeffery/timecop/pull/74
+class << Time
+  remove_method :zone
+end
