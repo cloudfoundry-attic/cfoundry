f3e4850dc7542863365ff90152952aeff38818f4
Nicole Sullivan & Will Read <pair+nsullivan+will@pivotallabs.com>
2013-05-20 17:18:01 -0700

    Add support for dashboard_url for service instances

--- lib/cfoundry/v2/service_instance.rb
+++ lib/cfoundry/v2/service_instance.rb
@@ -3,6 +3,7 @@ require "cfoundry/v2/model"
 module CFoundry::V2
   class ServiceInstance < Model
     attribute :name, :string
+    attribute :dashboard_url, :string
     to_one    :space
     to_one    :service_plan
     to_many   :service_bindings
